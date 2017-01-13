4230a9e0c2def7be28a19470e71d354260740809
Teemo <18720949926@163.com>
2017-01-13 10:12:58 +0800

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.sql.*;
+
+public class DB {
+	//”√ªß√˚£¨√‹¬Î
+	private static final String USER = "";
+	private static final String PASSWORD = "";
+	
+	public static Connection getConn() {
+		String URL="jdbc:mysql://localhost:3306/test";
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL,USER,PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void closeConn(Connection conn) {
+		try {
+			if(conn != null) {
+				conn.close();
+				conn = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void closeStmt(Statement stmt) {
+		try {
+			if(stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void closeRs(ResultSet rs) {
+		try {
+			if(rs != null) {
+				rs.close();
+				rs = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
