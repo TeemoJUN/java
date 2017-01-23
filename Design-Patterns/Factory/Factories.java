2226d2410b8c83de44f91db9866431c8ee263173
Teemo <18720949926@163.com>
2017-01-23 19:03:49 +0800

    Add files via upload

---
+++ Design-Patterns/Factory/Factories.java
@@ -0,0 +1,46 @@
+package Test_9;
+
+
+
+interface Service{
+	void method1();
+	void method2();
+}
+
+interface ServiceFactory{
+	Service getService();
+}
+
+class Implementation1 implements Service{
+	
+	Implementation1(){}
+	@Override
+	public void method1() {
+		System.out.println("Implementation1 method1");
+		
+	}
+
+	@Override
+	public void method2() {
+		System.out.println("Implementation2 method2");
+	}
+}
+
+class Implementation1Factory implements ServiceFactory{
+
+	@Override
+	public Service getService() {
+		return new Implementation1();
+	}
+}
+
+public class Factories {
+	public static void serviceConsumer(ServiceFactory fact){
+		Service s=fact.getService();
+		s.method1();
+		s.method2();
+	}
+	public static void main(String[] args){
+		serviceConsumer(new Implementation1Factory());
+	}
+}
